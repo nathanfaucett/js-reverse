2bc73dfd8e9fd6056f02530eb6cd8494f4bd6963
nathanfaucett <nathanfaucett@gmail.com>
2015-08-23 10:13:56 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,37 +1,11 @@
-var keys = require("keys"),
-    isArrayLike = require("is_array_like");
+var isArrayLike = require("is_array_like"),
+    arrayReverse = require("array-reverse"),
+    objectReverse = require("object-reverse");
 
 
 module.exports = reverse;
 
 
-function reverse(object) {
-    return isArrayLike(object) ? reverseArray(object) : reverseObject(Object(object));
-}
-
-function reverseArray(array) {
-    var i = array.length,
-        results = new Array(i),
-        j = 0;
-
-    while (i--) {
-        results[j++] = array[i];
-    }
-
-    return results;
-}
-
-function reverseObject(object) {
-    var objectKeys = keys(object),
-        i = -1,
-        il = objectKeys.length - 1,
-        results = {},
-        key;
-
-    while (i++ < il) {
-        key = objectKeys[i];
-        results[object[key]] = key;
-    }
-
-    return results;
+function reverse(value) {
+    return isArrayLike(value) ? arrayReverse(value) : objectReverse(Object(value));
 }
